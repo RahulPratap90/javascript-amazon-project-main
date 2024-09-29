d977996f502e26b21ae6623bb7da2259f302d4e6
RahulPratap90 <rahulpratap601@gmail.com>
2024-09-29 14:01:00 +0530

    commit new

--- Script/amazon.js
+++ Script/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product)=> {
                 Added
               </div>
 
-              <button class="add-to-cart-button button-primary js-add-to-cart">
+              <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
                 Add to Cart
               </button>
             </div>`;
@@ -59,7 +59,24 @@ innerHTML= productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=> {
-    console.log('Added Products');
-  })
+   const productId = button.dataset.productId;
+   let matchingItem;
 
+   cart.forEach((item)=>{
+    if(productId=== item.productId){
+      matchingItem = item;
+    }
+
+    });
+    if (matchingItem){
+      matchingItem.quantity +=1;
+    } else{
+      cart.push({
+        productId:productId,
+        quantity:1
+       });
+
+    }
+   console.log(cart);
+  });
 });
